aae069c1b3f1221235f6abefca78404bbf5d92de
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-01-21 11:12:13 +0530

    Update muditCode.c

--- DSA_ End-term Lab Exam (S1,S2,R1,R2 Batches_ 31-07-2021 at 12_05 PM- 2_05 PM)/muditCode.c
+++ DSA_ End-term Lab Exam (S1,S2,R1,R2 Batches_ 31-07-2021 at 12_05 PM- 2_05 PM)/muditCode.c
@@ -8,18 +8,9 @@ int main() {
     printf("Enter number of right rotations: ");
     scanf("%d", &K);
     
-    int arr[N];
-    printf("Enter the values in the array: ");
-    for(int i=0; i<N; i++){
-        scanf("%d ", &arr[i]);
-    }
+    K%=N;
+    printf("%d %d", N, K);
     
-    for(int i=N-K; i<N; i++){
-        printf("%d ", i);
-    }
-    for(int i=0; i<N-K; i++){
-        printf("%d ", i);
-    }
     
     return 0;
 }
